6f35bfece42cbbcfc4a02b34d40d6fe2d1145d30
Akhmetzhar Kabas <241146@astanait.edu.kz>
2026-01-18 03:22:43 +0500

    dada

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView, TemplateView 
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, TemplateView
 from django.db.models import Avg, Q
 from .models import Item
 from .forms import ReviewForm
@@ -10,28 +10,19 @@ class ItemListView(ListView):
     context_object_name = 'items'
 
     def get_queryset(self):
-        # Получаем данные из URL (и обычный поиск, и фильтры)
         query = self.request.GET.get('q')
-        genre = self.request.GET.get('genre')  # Новое: получаем жанр
-        
+        genre = self.request.GET.get('genre')
         queryset = Item.objects.annotate(avg_rating=Avg('reviews__rating'))
-        
-        # Фильтр по поисковому слову (Название или Описание)
         if query:
             queryset = queryset.filter(
-                Q(title__icontains=query) | Q(description__icontains=query)
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
             )
-            
-        # Новое: Фильтр по жанру (если выбран в расширенном поиске)
         if genre:
-            # Ищем жанр в описании или отдельном поле (если ты его создал)
             queryset = queryset.filter(description__icontains=genre)
-            
-        
         return queryset.order_by('-avg_rating')
 
     def get_context_data(self, **kwargs):
-        # Передаем значение поиска обратно в шаблон, чтобы показать "Результаты по запросу ..."
         context = super().get_context_data(**kwargs)
         context['query'] = self.request.GET.get('q')
         return context
@@ -43,13 +34,12 @@ class ItemDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = ReviewForm()  
+        context['form'] = ReviewForm()
         return context
 
     def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect('admin:login') 
-        
+            return redirect('admin:login')
         self.object = self.get_object()
         form = ReviewForm(request.POST)
         if form.is_valid():
@@ -59,6 +49,6 @@ class ItemDetailView(DetailView):
             review.save()
             return redirect('item_detail', pk=self.object.pk)
         return self.render_to_response(self.get_context_data(form=form))
-    
+
 class AdvancedSearchView(TemplateView):
     template_name = 'reviews/advanced_search.html'
